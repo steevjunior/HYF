7072293ce9ebf3db59cffde846bbe0719112cda2
bitomagira <bitomagira@gmail.com>
2019-02-26 14:36:45 +0100

    exo2

--- exo2/index.js
+++ exo2/index.js
@@ -1,11 +1,11 @@
 function askTask()
 {
-    let text=prompt("Please give a task");
-    let task=document.createElement("LI");
-    let textnode=document.createTextNode(text);
-    task.appendChild(textnode);
+    let taskName=prompt("Please give a task name");
+    let taskItem=document.createElement("LI");
+    let listNode=document.createTextNode(taskName);
+    taskItem.appendChild(listNode);
     let list=document.getElementById("taskList");
-    list.insertBefore(task,list.childNodes[0]);
+    list.insertBefore(taskItem,list.childNodes[0]);
     let nombre=document.getElementById("taskList").childElementCount;
     console.log(nombre)
     document.getElementById('nbreTask').innerText=nombre;  
